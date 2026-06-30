c7301560ded7cb74164ad5cd01eb2c52165bfed5
Saniya-khan26 <saniyakhan2632006@gmail.com>
2026-06-30 10:32:12 +0530

    Add files via upload
    toogle btn

---
+++ Toogle btn/toogle.js
@@ -0,0 +1,30 @@
+// let modeBtn=document.querySelector("#mode");
+// let currMode="light";
+// modeBtn.addEventListener("click",()=>{
+//    if (currMode ==="light"){
+//     currMode="dark";
+//     // document.querySelector("body").style.backgroundColor="black";
+//    }
+//    else{
+//     currMode="light";
+//     // document.querySelector("body").style.backgroundColor="white";
+//    }
+//    console.log(currMode);
+// });
+//===========another way===========
+let modeBtn=document.querySelector("#mode");
+let body=document.querySelector("body");
+let currMode="light";
+modeBtn.addEventListener("click",()=>{
+   if (currMode ==="light"){
+    currMode="dark";
+    body.classList.add("dark");
+    body.classList.remove("light");
+   }
+   else{
+    currMode="light";
+    body.classList.add("light");
+    body.classList.remove("dark")
+   }
+   console.log(currMode);
+});
